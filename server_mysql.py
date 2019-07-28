0a13229b956a0b69224650a37ea4208bd9bca084
Balaji Ramasubramanian <balaji.ramasubramanian@gmail.com>
2019-07-28 13:15:09 -0700

    Lots of good code here now.

--- server_mysql.py
+++ server_mysql.py
@@ -23,7 +23,42 @@ DELETE_RECORD = 'DELETE FROM table WHERE id=%(id)s'
 def mainpage():
     print(get_flashed_messages())
     session.clear()
+    flash("I just flashed", "success")
     return render_template("index.html")
+
+@app.route("/slow_response")
+def slow_response():
+    for i in range(10000000):
+        i+=1
+    return render_template("partial.html")
+
+usernames = [{'username': 'balaji'}, {'username': 'eliza'}]
+
+def existing_username(namestr):
+    for record in usernames:
+        if record['username'] == namestr:
+            return True
+    return False
+
+@app.route("/_username", methods=['POST'])
+def _username():
+    long_enough = len(request.form['username']) >= 5
+    is_available = not existing_username(request.form['username'])
+    return render_template("partials/_username.html", long_enough=long_enough, is_available=is_available)
+
+def is_complex_password(text):
+    return my_utils.AT_LEAST_ONE_NUM_REGEX.match(text) and my_utils.AT_LEAST_ONE_CAP_REGEX.match(text)
+
+@app.route('/_password', methods=['POST'])
+def _password():
+    long_enough = len(request.form['password']) >= 8
+    complex_enough = is_complex_password(request.form['password'])
+    return render_template("partials/_password.html", long_enough=long_enough, complex_enough=complex_enough)
+
+@app.route('/_confirm', methods=['POST'])
+def _confirm():
+    confirmation = request.form['password'] == request.form['confirm']
+    return render_template("partials/_confirm.html", confirmation=confirmation)
 
 if __name__ == "__main__":
     app.run(debug=True)
